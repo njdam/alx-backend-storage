ea72e60637ffe444e4129bb1454fc9d430300e10
njdam <jeandamngaboyimana@gmail.com>
2024-03-21 22:26:58 +0000

    In and not out!

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,6 @@
 -- SQL script that creates a table users with the requirements
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
