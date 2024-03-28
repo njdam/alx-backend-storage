e7cf4a6a633b6ecb9c45921fcd28729ea7a3dd3f
njdam <jeandamngaboyimana@gmail.com>
2024-03-28 04:00:49 +0000

    Retrieving lists!

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -86,6 +86,19 @@ def call_history(method: Callable) -> Callable:
 
 def replay(fn: Callable) -> None:
     """
+    In this tasks, we will implement a replay function to display the
+    history of calls of a particular function.
+
+    Use keys generated in previous tasks to generate the following output:
+    `>>> cache = Cache()`
+    `>>> cache.store("foo")`
+    `>>> cache.store("bar")`
+    `>>> cache.store(42)`
+    `>>> replay(cache.store)`
+        Cache.store was called 3 times:
+        Cache.store(*('foo',)) -> 13bf32a9-a249-4664-95fc-b1062db2038f
+        Cache.store(*('bar',)) -> dcddd00c-4219-4dd7-8877-66afbe8e7df8
+        Cache.store(*(42,)) -> 5e752f2b-ecd8-4925-a3ce-e2efdee08d20
     """
     if fn is None or not hasattr(fn, '__self__'):
         return
