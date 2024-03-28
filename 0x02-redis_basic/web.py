8e2c590a798c55045857853ad3b9c3f9ab1984ba
njdam <jeandamngaboyimana@gmail.com>
2024-03-28 05:38:47 +0000

    Implementing an expiring web cache and tracker!

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python3
+'''A module with tools for request caching and tracking.
+'''
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
+
+
+'''
+#!/usr/bin/env python3
 """
 A module with tools for request caching and tracking.
 """
@@ -34,10 +75,9 @@ def data_cacher(func: Callable) -> Callable:
         # Call the original function
         result = func(url)
 
-        redis_client.set(f'count:{url}', 0)
-
         # Cache the result with expiration time of 10 seconds
         redis_client.setex(f'result:{url}', 10, result)
+        redis_client.set(f'count:{url}', 0)
 
         return result
     return wrapper
@@ -50,3 +90,4 @@ def get_page(url: str) -> str:
     and tracking the request.
     """
     return requests.get(url).text
+'''
